578d7df9c4abfe5e0354796c85336a8825ed4af1
JhonnyBravo <sanfranceshika5@gmail.com>
2020-06-13 00:57:43 +0900

    modified: src/main/java/java_itamae_g_auth/domain/repository/credential/CredentialRepositoryImpl.java
    new method: createWebAppCredential
    renamed method: create -> createInstalledAppCredential

--- src/main/java/java_itamae_g_auth/domain/repository/credential/CredentialRepositoryImpl.java
+++ src/main/java/java_itamae_g_auth/domain/repository/credential/CredentialRepositoryImpl.java
@@ -6,6 +6,8 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets.Details;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.util.store.FileDataStoreFactory;
 
 import java_itamae_g_auth.domain.model.AuthenticationAttribure;
@@ -13,8 +15,8 @@ import java_itamae_g_auth.domain.model.AuthenticationAttribure;
 public class CredentialRepositoryImpl implements CredentialRepository {
 
     @Override
-    public Credential create(AuthenticationAttribure authAttr)
-            throws Exception {
+    public Credential createInstalledAppCredential(
+            AuthenticationAttribure authAttr) throws Exception {
         FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(
                 new File("credential"));
 
@@ -28,6 +30,23 @@ public class CredentialRepositoryImpl implements CredentialRepository {
 
         Credential credential = authorizationCodeInstalledApp
                 .authorize(authAttr.getUserName());
+        credential.refreshToken();
+
+        return credential;
+    }
+
+    @Override
+    public Credential createWebAppCredential(AuthenticationAttribure authAttr)
+            throws Exception {
+        Details details = authAttr.getClientSecrets().getDetails();
+        String accessToken = authAttr.getTokenResponse().getAccessToken();
+
+        Credential credential = new GoogleCredential.Builder()
+                .setClientSecrets(details.getClientId(),
+                        details.getClientSecret())
+                .setJsonFactory(authAttr.getJsonFactory())
+                .setTransport(authAttr.getHttpTransport()).build();
+        credential.setAccessToken(accessToken);
         credential.refreshToken();
 
         return credential;

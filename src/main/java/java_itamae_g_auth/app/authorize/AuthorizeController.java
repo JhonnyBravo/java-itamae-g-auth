6ec140b4ef6f1d53867f2bceda9626a899ed83f0
JhonnyBravo <sanfranceshika5@gmail.com>
2020-06-13 01:04:57 +0900

    modified: src/main/java/java_itamae_g_auth/app/authorize/AuthorizeController.java
    認証処理を AuthenticationService へ移動。

--- src/main/java/java_itamae_g_auth/app/authorize/AuthorizeController.java
+++ src/main/java/java_itamae_g_auth/app/authorize/AuthorizeController.java
@@ -11,64 +11,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-
 import java_itamae_g_auth.domain.model.AuthenticationAttribure;
+import java_itamae_g_auth.domain.service.authentication.AuthenticationService;
+import java_itamae_g_auth.domain.service.authentication.AuthenticationServiceImpl;
 
 /**
- * Servlet implementation class AuthorizeController
+ * 認証処理のデモページ
  */
 @WebServlet("/authorize")
 public class AuthorizeController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private final AuthenticationService service;
+
     public AuthorizeController() {
         super();
+        service = new AuthenticationServiceImpl();
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * 認証処理を実行し、取得した Credential を画面へ送信する。
+     *
+     * @param request
+     *            {@link javax.servlet.http.HttpServletRequest}
+     * @param response
+     *            {@link javax.servlet.http.HttpServletResponse}
+     * @throws ServletException
+     *             {@link javax.servlet.ServletException}
+     * @throws IOException
+     *             {@link java.io.IOException}
      */
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        String code = request.getParameter("code");
-
+        // セッションを通じて認証情報を受け取る。
         HttpSession session = request.getSession();
         AuthenticationAttribure authAttr = (AuthenticationAttribure) session
                 .getAttribute("authAttr");
+        // リクエストパラメータからレスポンスコードを受け取る。
+        String code = request.getParameter("code");
+        authAttr.setCode(code);
 
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                authAttr.getHttpTransport(), authAttr.getJsonFactory(),
-                authAttr.getClientSecrets(), authAttr.getScopeList())
-                        .setAccessType("offline").setApprovalPrompt("force")
-                        .build();
+        // 認証処理を実行する。
+        try {
+            service.authorizeWebApp(authAttr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        String redirectUrl = String.join(",",
-                authAttr.getClientSecrets().getWeb().getRedirectUris());
-        GoogleTokenResponse tokenResponse = flow.newTokenRequest(code)
-                .setRedirectUri(redirectUrl).execute();
-
-        Credential credential = new GoogleCredential.Builder()
-                .setClientSecrets(
-                        authAttr.getClientSecrets().getDetails().getClientId(),
-                        authAttr.getClientSecrets().getDetails()
-                                .getClientSecret())
-                .setJsonFactory(authAttr.getJsonFactory())
-                .setTransport(authAttr.getHttpTransport()).build();
-
-        credential.setAccessToken(tokenResponse.getAccessToken());
-        credential.refreshToken();
-
-        authAttr.setCredential(credential);
-        request.setAttribute("credential", credential);
+        request.setAttribute("credential", authAttr.getCredential());
 
         ServletContext context = getServletContext();
         RequestDispatcher dispatcher = context
